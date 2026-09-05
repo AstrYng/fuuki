c37266df7139858142b035d63f69fa370c1ae8da
fengqi <yong77721@gmail.com>
2026-09-06 01:28:14 +0800

    chore: update theme color

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	// TODO: Add a configurable timezone, e.g. "Asia/Shanghai".
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 190, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
