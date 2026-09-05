830333082de8aeb2134cf757904e4fe4eade97cb
fengqi <yong77721@gmail.com>
2026-09-05 23:03:36 +0800

    chore: configure basic site settings

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,10 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "Fuuki",
+	subtitle: "行于山海，写于风起",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	// TODO: Add a configurable timezone, e.g. "Asia/Shanghai".
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -54,8 +55,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "风起",
+	bio: "行于山海，写于风起。",
 	links: [
 		{
 			name: "Twitter",
