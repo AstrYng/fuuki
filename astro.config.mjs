f9f8a4a55abf93b49376435a6c4e889a12ae6f97
fengqi <yong77721@gmail.com>
2026-09-05 23:08:51 +0800

    chore: configure site and RSS URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://blog.asteryng.com/",
+	//site: "https://blog.fuuki.com/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
